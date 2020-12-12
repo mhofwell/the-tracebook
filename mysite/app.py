a01abf85e074304a0c598e2fa1799fd30f5fcb10
Michael Hofweller <michael.hofweller@gmail.com>
2020-12-11 16:04:48 -0800

    templates!

--- mysite/app.py
+++ mysite/app.py
@@ -70,17 +70,55 @@ def register():
     if request.method == "GET":
         return render_template("auth/register.html")
 
+    """DO THIS NEXT"""
+    # else:
+    #     username = request.form.get("username")
+    #     password = request.form.get("password")
+    #     confirmation = request.form.get("confirmation")
+    #     # if username is already in db don't register!
+    #     if password != confirmation or not password or not confirmation:
+    #         return apology("Sorry passwords don't match!", 403)
+    #     else:
+    #         pwHash = generate_password_hash(password)
+    #         db.execute("INSERT INTO users (username, hash) VALUES(:username, :hash)",
+    #                     username=username, hash=pwHash)
+    #         return redirect("/login")
+
 
 @app.route("/login.html", methods=["GET", "POST"])
-@login_required
 def login():
-    # make info into a dict with those tips?
-    cur = get_db().cursor()
-    rows = cur.execute()
-    for row in rows:
-        print(row)
     if request.method == "GET":
         return render_template("auth/login.html")
+    else:
+        # Forget any user_id
+        session.clear()
+
+        # User reached route via POST (as by submitting a form via POST)
+        if request.method == "POST":
+
+            """DO THIS NEXT"""
+
+            # # Ensure username was submitted
+            # if not request.form.get("username"):
+            #         return apology("must provide username", 403)
+
+            #     # Ensure password was submitted
+            #     elif not request.form.get("password"):
+            #         return apology("must provide password", 403)
+
+            #     # Query database for username
+            #     rows = db.execute("SELECT * FROM users WHERE username = :username",
+            #                     username=request.form.get("username"))
+
+            #     # Ensure username exists and password is correct
+            #     if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
+            #         return apology("invalid username and/or password", 403)
+
+            #     # Remember which user has logged in
+            #     session["user_id"] = rows[0]["id"]
+
+            #     # Redirect user to home page
+            #     return redirect("/")
 
 
 @app.route("/logout.html", methods=["GET", "POST"])
@@ -89,73 +127,6 @@ def logout():
     if request.method == "GET":
         session.clear()
         return redirect("/")
-
-        # Connect the DB to sqlite3 for this application!
-
-        # @app.route("/login", methods=["GET", "POST"])
-        # def login():
-        #     """Log user in"""
-
-        #     # Forget any user_id
-        #     session.clear()
-
-        #     # User reached route via POST (as by submitting a form via POST)
-        #     if request.method == "POST":
-
-        #         # Ensure username was submitted
-        #         if not request.form.get("username"):
-        #             return apology("must provide username", 403)
-
-        #         # Ensure password was submitted
-        #         elif not request.form.get("password"):
-        #             return apology("must provide password", 403)
-
-        #         # Query database for username
-        #         rows = db.execute("SELECT * FROM users WHERE username = :username",
-        #                           username=request.form.get("username"))
-
-        #         # Ensure username exists and password is correct
-        #         if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
-        #             return apology("invalid username and/or password", 403)
-
-        #         # Remember which user has logged in
-        #         session["user_id"] = rows[0]["id"]
-
-        #         # Redirect user to home page
-        #         return redirect("/")
-
-        #     # User reached route via GET (as by clicking a link or via redirect)
-        #     else:
-        #         return render_template("login.html")
-
-        # @app.route("/logout")
-        # def logout():
-        #     """Log user out"""
-
-        #     # Forget any user_id
-        #     session.clear()
-
-        #     # Redirect user to login form
-        #     return redirect("/")
-
-        # @app.route("/register", methods=["GET", "POST"])
-        # def register():
-        #     if request.method == "GET":
-        #         return render_template("register.html")
-        #     else:
-        #         username = request.form.get("username")
-        #         password = request.form.get("password")
-        #         confirmation = request.form.get("confirmation")
-        #         # if username is already in db don't register!
-        #         if password != confirmation or not password or not confirmation:
-        #             return apology("Sorry passwords don't match!", 403)
-        #         else:
-        #             pwHash = generate_password_hash(password)
-        #             db.execute("INSERT INTO users (username, hash) VALUES(:username, :hash)",
-        #                        username=username, hash=pwHash)
-        #             return redirect("/login")
-
-        #             return redirect("/")
 
 
 def errorhandler(e):
@@ -168,3 +139,9 @@ def errorhandler(e):
 # Listen for errors
 for code in default_exceptions:
     app.errorhandler(code)(errorhandler)
+
+    # make info into a dict with those tips?
+    # cur = get_db().cursor()
+    # rows = cur.execute()
+    # for row in rows:
+    #     print(row)
