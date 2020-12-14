0ea3039274c744acca5cc1bfbb0c5e2ea7a693e7
Michael Hofweller <michael.hofweller@gmail.com>
2020-12-13 16:16:38 -0800

    templates

--- mysite/app.py
+++ mysite/app.py
@@ -30,18 +30,36 @@ def get_db():
     db = getattr(g, '_database', None)
     if db is None:
         db = g._database = sqlite3.connect(DATABASE)
+        db.row_factory = sqlite3.Row
     return db
+
+# set up query helper
+
+
+def query_db(query, args=(), one=False):
+    cursor = get_db().execute(query, args)
+    row_tuples = cursor.fetchall()
+    cursor.close()
+    return (row_tuples[0] if row_tuples else None) if one else row_tuples
+
+# use of query helper
+# https://www.kite.com/python/docs/sqlite3.Connection.row_factory
+
+# for user in query_db('select * from users'):
+#     print user['username'], 'has the id', user['user_id']
 
 
 # ensure database closes after the application request ends by watching the application-level data
+
+
 @app.teardown_appcontext
 def close_connection(exception):
     db = getattr(g, '_database', None)
     if db is not None:
         db.close()
 
-# Ensure responses aren't cached
 
+# Ensure responses aren't cached
 
 @app.after_request
 def after_request(response):
@@ -85,40 +103,45 @@ def register():
     #         return redirect("/login")
 
 
+@app.route("/account.html", methods=["GET", "POST"])
+def account():
+    if request.method == "GET":
+        return render_template("acc/account.html")
+
+
 @app.route("/login.html", methods=["GET", "POST"])
 def login():
     if request.method == "GET":
         return render_template("auth/login.html")
-    else:
-        # Forget any user_id
-        session.clear()
+    # else:
+    #     # Forget any user_id
+    #     session.clear()
 
-        # User reached route via POST (as by submitting a form via POST)
-        if request.method == "POST":
+    #     # User reached route via POST (as by submitting a form via POST)
+    #     if request.method == "POST":
 
-            """DO THIS NEXT"""
+    #         # Ensure username was submitted
+    #         if not request.form.get("username"):
+    #                 return apology("must provide username", 403)
 
-            # # Ensure username was submitted
-            # if not request.form.get("username"):
-            #         return apology("must provide username", 403)
+    #             # Ensure password was submitted
+    #             elif not request.form.get("password"):
+    #                 return apology("must provide password", 403)
 
-            #     # Ensure password was submitted
-            #     elif not request.form.get("password"):
-            #         return apology("must provide password", 403)
+    #             """ Redo this for SQLite3 connection with cursor. """
+    #             # cur = get_db().cursor()
+    #             # rows = get_db().cursor("SELECT * FROM users WHERE username = :username",
+    #             #                 username=request.form.get("username"))
 
-            #     # Query database for username
-            #     rows = db.execute("SELECT * FROM users WHERE username = :username",
-            #                     username=request.form.get("username"))
+    #             # Ensure username exists and password is correct
+    #             if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
+    #                 return apology("invalid username and/or password", 403)
 
-            #     # Ensure username exists and password is correct
-            #     if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
-            #         return apology("invalid username and/or password", 403)
+    #             # Remember which user has logged in
+    #             session["user_id"] = rows[0]["id"]
 
-            #     # Remember which user has logged in
-            #     session["user_id"] = rows[0]["id"]
-
-            #     # Redirect user to home page
-            #     return redirect("/")
+    #             # Redirect user to home page
+    #             return redirect("/")
 
 
 @app.route("/logout.html", methods=["GET", "POST"])
