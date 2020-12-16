725c8ae55d012c7abc6b7984802d208d99d9966e
Michael Hofweller <michael.hofweller@gmail.com>
2020-12-16 15:48:27 -0800

    routing

--- myapp/app.py
+++ myapp/app.py
@@ -26,7 +26,9 @@ Create better database helper functions that ensure connections are closed and o
 
 Make notes on csrf and flask_wtf
 
+DO THE MEGA FLASK TUTORIAL
 
+"""
 
 
 # Denote this moduile as the application
@@ -52,23 +54,32 @@ DATABASE = "contact_trace.db"
 conn = sqlite3.connect(DATABASE)
 c = conn.cursor()
 
-# def get_db():
-#     db = getattr(g, '_database', None)
-#     if db is None:
-#         db = g._database = sqlite3.connect(DATABASE)
-#         db = db.cursor()
-#     return db
-
-# # set up query helper
-
-#     # db.row_factory = sqlite3.Row
+# ensure database closes after the application request ends by watching the application-level data
 
 
-# def query_db(query, args=(), one=False):
-#     cursor = get_db().execute(query, args)
-#     row_tuples = cursor.fetchall()
-#     cursor.close()
-#     return (row_tuples[0] if row_tuples else None) if one else row_tuples
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+        db = db.cursor()
+        db.row_factory = sqlite3.Row
+    return db
+
+# set up query helper
+
+
+def query_db(query, args=(), one=False):
+    cursor = get_db().execute(query, args)
+    row_tuples = cursor.fetchall()
+    cursor.close()
+    return (row_tuples[0] if row_tuples else None) if one else row_tuples
 
 # use of query helper
 # https://www.kite.com/python/docs/sqlite3.Connection.row_factory
@@ -77,22 +88,11 @@ c = conn.cursor()
 #     print user['username'], 'has the id', user['user_id']
 
 
-# ensure database closes after the application request ends by watching the application-level data
-
-
 # prepare some reference data for accounts
-
 prov_list = ["AB", "BC", "MB", "ON", "QB",
              "SK", "NFLD", "NS", "QB", "NWT", "YK"]
 
 prov_length = len(prov_list)
-
-
-@app.teardown_appcontext
-def close_connection(exception):
-    db = getattr(g, '_database', None)
-    if db is not None:
-        db.close()
 
 
 # Ensure responses aren't cached
@@ -107,6 +107,7 @@ def after_request(response):
 
 @app.route("/", methods=["GET"])
 def home():
+    session.clear()
     if request.method == "GET":
         # userId = session["user_id"]
         return render_template("index.html")
@@ -129,16 +130,11 @@ def register():
         else:
             password = request.form.get("password")
             confirmation = request.form.get("confirmation")
-            print(password)
-            print(confirmation)
             # if username is already in db don't register!
             if password != confirmation or not password or not confirmation:
                 return apology("Sorry passwords don't match!", 403)
             else:
                 pwHash = generate_password_hash(password)
-                print(firstname)
-                print(lastname)
-                print(pwHash)
                 insert_user = "INSERT INTO users (firstname, lastname, email, hash) VALUES(?, ?, ?, ?)"
                 values = (firstname, lastname, email, pwHash)
                 c.execute(insert_user, values)
@@ -166,6 +162,7 @@ def get_tracebook_form():
 
 
 @app.route("/account.html", methods=["GET", "POST"])
+# @login_required
 def account():
     if request.method == "GET":
         return render_template("acc/account.html", provinces=prov_list, length=prov_length)
@@ -184,35 +181,34 @@ def settings():
 def login():
     if request.method == "GET":
         return render_template("auth/login.html")
-    # else:
-    #     # Forget any user_id
-    #     session.clear()
+    else:
+        # Forget any user_id
+        session.clear()
 
-    #     # User reached route via POST (as by submitting a form via POST)
-    #     if request.method == "POST":
+        # Ensure username was submitted
+        if not request.form.get("email"):
+            return apology("must provide email!", 403)
 
-    #         # Ensure username was submitted
-    #         if not request.form.get("username"):
-    #                 return apology("must provide username", 403)
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return apology("must provide password", 403)
 
-    #             # Ensure password was submitted
-    #             elif not request.form.get("password"):
-    #                 return apology("must provide password", 403)
+        else:
+            query = ("SELECT * FROM users WHERE email = ?")
+            form_email = request.form.get("email")
+            email_query = (form_email,)
+            user = query_db(query, email_query)
 
-    #             """ Redo this for SQLite3 connection with cursor. """
-    #             # cur = get_db().cursor()
-    #             # rows = get_db().cursor("SELECT * FROM users WHERE username = :username",
-    #             #                 username=request.form.get("username"))
+            print(user[0]['firstname'])
 
-    #             # Ensure username exists and password is correct
-    #             if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
-    #                 return apology("invalid username and/or password", 403)
+            if user[0]["email"] != form_email or not check_password_hash(user[0]["hash"], request.form.get("password")):
+                return apology("User not found!", 403)
 
-    #             # Remember which user has logged in
-    #             session["user_id"] = rows[0]["id"]
-
-    #             # Redirect user to home page
-    #             return redirect("/")
+            # Remember which user has logged in
+            session["user_id"] = user[0]["_id"]
+            firstname = user[0]["firstname"]
+            # Redirect user to account page
+            return render_template("acc/account.html", name=firstname, provinces=prov_list, length=prov_length)
 
 
 @app.route("/logout.html", methods=["GET", "POST"])
